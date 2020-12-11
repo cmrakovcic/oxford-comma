79046a776e87c9b144bfe78cd175ea6b447c5ea5
Christopher Mrakovcic <cmrakovcic@aol.com>
2020-12-10 19:31:56 -0500

    commit tree 0d4f0a5b035a2f97693dfa068992bb90401bbe0c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    comma_items = ""
+    if array.size <= 2
+        return array.size === 1 ? array[0] : "#{array[0]} and #{array[1]}"
+    end
+    array.each{|item| 
+      comma_items += item === array[-1] ? "and #{item}" : "#{item}, "
+    }
+    return comma_items
+  end
